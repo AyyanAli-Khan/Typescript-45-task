7fcde668d6edd24dbff03ed2730022819000da9d
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-05-11 23:49:32 +0500

    add 3-6

---
+++ Q4  Famous Quote/main.js
@@ -0,0 +1,1 @@
+console.log('Quaid-e-Azam once said,"Pakistan is proud of her youth, particularly the students who have always been in the forefront in the hour of trail and need".');
---
+++ Q5  Famous Quote 2/main.js
@@ -0,0 +1,3 @@
+var famous_person = " Muhammad Ali ";
+var message = "He who is not courageous enough to take risks will accomplish nothing in life...";
+console.log("".concat(famous_person, "once said ,").concat(message));
