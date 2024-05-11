fe6fe683ca504575f47c136df41953fdc2257836
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-05-11 23:52:53 +0500

    add task no. 9

---
+++ Q9  Favorite Number/main.js
@@ -0,0 +1,3 @@
+var favourite_num = 12;
+var message = "I guess your favourite number is ".concat(favourite_num);
+console.log(message);
---
+++ Q9  Favorite Number/main.ts
@@ -0,0 +1,5 @@
+let favourite_num : number = 12 ;
+
+let message : string = `I guess your favourite number is ${favourite_num}`;
+
+console.log(message)
